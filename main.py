1a9db82922dc0c8ecaa0cef622bc05d61673763d
Kamomille <camille.bayon-de-noyer@esme.fr>
2022-03-23 10:47:04 +0100

    test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# This is a sample Python script.
+
+#import auchan
+
+import pandas as pd
+
+df = pd.read_csv("auchan_produits.csv", sep=';')
+
+df.prix = df.prix.str.replace(',', '.')
+df.prix = df.prix.astype(float)
+
+print(df.prix.mean())
+
