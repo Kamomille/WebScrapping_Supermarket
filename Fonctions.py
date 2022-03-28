7537b4f5134d66e74f9f91a3857188320d3b68da
sonia <soniamoghraoui@hotmail.fr>
2022-03-28 11:20:22 +0200

    Fonctions à utiliser dans les fichiers Python

--- Fonctions.py
+++ Fonctions.py
@@ -7,18 +7,39 @@ def Mettre_en_liste(element):
         Liste.append(element[a].text.replace('\n', ', '))
     return Liste
 
-#Rechercher les code postales et communes et les mettre dans une liste
-def code_Postal(Liste_Postal):
-    Code_Postal = []
-    for Postal in Liste_Postal:
-        regex = re.compile(", [0-9]{5}(.*)$")
-        resultat = regex.search(Postal)
-        resultat = resultat.group(0).replace("Cedex", "")
-        resultat = resultat.replace(", ", "")
-        if resultat is None:
-            print (None)
-        else:
-            Code_Postal.append(resultat)
-    len(Code_Postal)
-    return Code_Postal
 
+#Rechercher les code postales et communes et les mettre dans une liste
+""" 
+a permet de recuperer une partie du pattern
+a = 0 -> Tout le pattern
+a = 1 -> Seulement de code postal
+a = 2 -> Seulement la commune
+"""
+
+def Format_Postal_Commune(Donnees, a):
+    if a == 0:
+        Code_Postal = []
+        for Postal in Donnees:
+            regex = re.compile("([0-9]{5}) ((.*)$)")
+            resultat = regex.search(Postal)
+            resultat = resultat.group(a).replace("Cedex", "")
+            if resultat is None:
+                print (None)
+            else:
+                Code_Postal.append(resultat)
+        len(Code_Postal)
+        return Code_Postal
+    else:
+        regex = re.compile("([0-9]{5}) ((.*)$)")
+        resultat = regex.search(str(Donnees))
+        if resultat is None:
+            print(None)
+        else:
+            resultat = resultat.group(a)
+            return resultat
+
+def append(Location, Lat, Long):
+    print((Location.latitude, Location.longitude))
+    Lat.append(Location.latitude)
+    Long.append(Location.longitude)
+    return Lat, Long
