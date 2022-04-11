8109809c1b236446c3224de99f293b7be31cd94f
noot1402 <noor.majed@esme.fr>
2022-04-11 20:06:49 +0200

    code casino uwu

--- casinotest.py
+++ casinotest.py
@@ -22,40 +22,53 @@ button = WebDriverWait(driver, 100).until(
 time.sleep(2)
 y = 50
 
-def scrolltoBottom():
+def scrolltoBottom(li):
     time.sleep(3)
     y = 1000
-    for timer in range(0, 40):
-        driver.execute_script("window.scrollTo(0, " + str(y) + ")")
-        y += 1000
-        time.sleep(1)
-
-def getNbrProduits():
+    if (li==4 or li==3) :
+        for timer in range(0, 50):
+            driver.execute_script("window.scrollTo(0, " + str(y) + ")")
+            y += 1000
+            time.sleep(1)
+    if (li==11 or li==10) :
+        for timer in range(0, 100):
+            driver.execute_script("window.scrollTo(0, " + str(y) + ")")
+            y += 1000
+            time.sleep(1)
+def getNbrProduitsFruits(li,sr):
     try:
-        popup_close = WebDriverWait(driver, 10).until(
+        popup_close = WebDriverWait(driver, 5).until(
             EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div/div[2]/div/div/div[1]/a'))).click()
     except WebDriverException:
         print("0")
-        time.sleep(2)
+        #time.sleep(2)
     burger_menu = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/header/div[2]/div[3]"))).click()
-    nom_cat=driver.find_element_by_xpath("/html/body/div[2]/header/nav/div/ul/li[4]/div/div/div").text
+    time.sleep(3)
+    nom_cat=driver.find_element_by_xpath("/html/body/div[2]/header/nav/div/ul/li["+str(li)+"]/div/div/div").text
     print(nom_cat)
-    rayon = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/header/nav/div/ul/li[4]/div"))).click()
-    l_sous_rayons = driver.find_elements_by_xpath("//div[@class='list-container']")
-    for sr in range(1, len(l_sous_rayons)):
-        print(sr)
-        sous_rayon = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "/html/body/div[2]/header/nav/div/ul/li[4]/ul/div/li[" + str(sr) + "]/div"))).click()
-        click_rayon = WebDriverWait(driver, 100).until(
-            EC.element_to_be_clickable(
-                (By.XPATH,
-                 "/html/body/div[2]/header/nav/div/ul/li[4]/ul/div/li[" + str(sr) + "]/ul/div/div/div[3]/a"))).click()
-        scrolltoBottom()
-        list_nbr_produits = driver.find_elements_by_class_name("product-item__bottom")
-        return len(list_nbr_produits)
+    time.sleep(3)
+    if li == 4:
+        if nom_cat=="Fruits et légumes":
+            li=4
+        else:
+            li=3
+    if li==11:
+        if nom_cat=="Épicerie sucrée":
+            li=11
+        else:
+            li=10
+    rayon = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/header/nav/div/ul/li["+str(li)+"]/div"))).click()
+    sous_rayon = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, "/html/body/div[2]/header/nav/div/ul/li["+str(li)+"]/ul/div/li[" + str(sr) + "]/div"))).click()
+    click_rayon = WebDriverWait(driver, 100).until(
+        EC.element_to_be_clickable(
+            (By.XPATH,
+             "/html/body/div[2]/header/nav/div/ul/li["+str(li)+"]/ul/div/li[" + str(sr) + "]/ul/div/div/div[3]/a"))).click()
+    scrolltoBottom(li)
+    list_nbr_produits = driver.find_elements_by_class_name("product-item__bottom")
+    return len(list_nbr_produits)
 
 def Openheader():
     button = driver.find_elements("xpath", "//button[@class='accordion__header accordion__header--no-shadow']")
@@ -67,31 +80,43 @@ def OpenLinkInTab(link):
     driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
     New_window = driver.window_handles[1]
     driver.switch_to.window(New_window)
-    time.sleep(3)
-
+    time.sleep(2)
 
 def CloseTab():
     driver.close()
     main_window = driver.window_handles[0]
     driver.switch_to.window(main_window)
 
-
 def GetLinkShop():
     LineAction = driver.find_elements(by="xpath", value="//div[@class='store-line__actions']")
     liens_adresses = [i.find_element(by='css selector', value="a") for i in LineAction]
-    for lien in liens_adresses[3:]:
+    print(len(liens_adresses))
+    for lien in liens_adresses[150:]:
         OpenLinkInTab(lien)
         mag=driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[1]/div/ol/li[4]/span").text
-        print(mag)
-        # mag = driver.find_element_by_class_name(
-        # "base-breadcrumb__item base-breadcrumb__item--text base-breadcrumb__item--last base-breadcrumb__item--white").text
         driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div/a").click()
-        n=getNbrProduits()
-        #mag=getNomMag()
-        getCSV(n, mag)
+        n=getNbrProduitsFruits(4,1)
+        writer=createCSV(mag)
+        getCSV(n, mag,writer)
+        n=getNbrProduitsFruits(4,2)
+        getCSV(n, mag,writer)
+        n = getNbrProduitsFruits(11, 1)
+        getCSV(n, mag, writer)
+        n = getNbrProduitsFruits(11, 2)
+        getCSV(n, mag, writer)
+        n = getNbrProduitsFruits(11, 3)
+        getCSV(n, mag, writer)
+        n = getNbrProduitsFruits(11, 4)
+        getCSV(n, mag, writer)
+        n = getNbrProduitsFruits(11, 5)
+        getCSV(n, mag, writer)
+        n = getNbrProduitsFruits(11, 6)
+        getCSV(n, mag, writer)
         CloseTab()
-
+'''
 def getNomMag():
+/html/body/div[1]/div[1]/div[4]/div/div[65]/div/div/button
+/html/body/div[1]/div[1]/div[4]/div/div[65]/div/div/div/div[1]/div[2]/a[1]
      time.sleep(3)
      y = 0
      driver.execute_script("window.scrollTo(0, " + str(y) + ")")
@@ -99,21 +124,18 @@ def getNomMag():
      nom_mag = driver.find_element_by_xpath("/html/body/div[2]/header/div[1]/div/div/ul/li[2]/a/span[2]/em").text
      print(nom_mag)
      return nom_mag
-
-
-
-def getCSV(nbr_produits, nom_mag):
-    csvFile = open("csv_casino/casino_" + nom_mag + ".csv", "w", newline='', encoding="utf-8")
-    csvWriter = csv.writer(csvFile, delimiter=';', quotechar='"')
+'''
+def getCSV(nbr_produits, nom_mag,csvWriter):
+    #csvFile = open("casino_csv/casino_" + nom_mag + ".csv", "a", newline='', encoding="utf-8")
+    #csvWriter = csv.writer(csvFile, delimiter=';', quotechar='"')
     # -- Création du nom des colonnes --
-    csvWriter.writerow(['categorie', 'type', 'nom', 'poids', 'prix_par_kg', 'prix', 'promo'])
-
+    #csvWriter.writerow(['categorie', 'type', 'nom', 'poids', 'prix_par_kg', 'prix', 'promo'])
     for i in range(1, nbr_produits+1):
-        print(i)
         csv_fill = []
-        get_cat_name = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div/article/div[1]/div/div/span").text
+        get_cat_name = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div/article/div[1]/div/ol/li[1]/a").text
         csv_fill.append(get_cat_name)
-        csv_fill.append("null")
+        get_type_name=driver.find_element_by_xpath("/html/body/div[2]/div[3]/div/div/article/div[1]/div/div/span").text
+        csv_fill.append(get_type_name)
         get_it_name = driver.find_elements_by_xpath(
             "/html/body/div[2]/div[3]/div/div/article/div[3]/ul/li[" + str(i) + "]/section/div[2]/a")
         for element in get_it_name:
@@ -151,7 +173,11 @@ def getCSV(nbr_produits, nom_mag):
         except WebDriverException:
             csv_fill.append("non")
         csvWriter.writerow((csv_fill))
-
+def createCSV(nom_mag):
+    csvFile = open("casino_csv/casino_" + nom_mag + ".csv", "a", newline='', encoding="utf-8")
+    csvWriter = csv.writer(csvFile, delimiter=';', quotechar='"')
+    csvWriter.writerow(['categorie', 'type', 'nom', 'poids', 'prix_par_kg', 'prix', 'promo'])
+    return csvWriter
 #                                       SELECTIONS DRIVE
 Openheader()
 GetLinkShop()
